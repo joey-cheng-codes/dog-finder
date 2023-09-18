81ddf88dc2b6566e1c178e2e3019660da35c5694
joey-cheng-codes <j.cheng93@gmail.com>
2023-09-18 18:55:05 -0400

    modifiying unused tailwind files

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/login' element={<Login />} />
-        <Route path="*" element={<NoMatch />} />
+        {/* <Route path="*" element={<NoMatch />} /> */}
       </Routes>
     </div>
   )
