6496db6fa660f1aa52b298a437ec906c8c34861c
joey-cheng-codes <j.cheng93@gmail.com>
2023-09-29 17:12:38 -0400

    breaking out toggle into its own component

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import CardHolder from '../CardHolder/CardHolder.jsx';
 import Match from '../Match/Match.jsx';
 import Pagination from '../Pagination/Pagination.jsx';
+import Toggle from '../Toggle/Toggle.jsx';
 import 'flowbite';
 
 const Filter = () => {
@@ -281,17 +282,7 @@ const Filter = () => {
 
   return (
     <div >
-      {/* toggle for sorting starts */}
-      <div className='toggle-container'>
-        <label className="relative inline-flex items-center cursor-pointer" >
-          <input type="checkbox" value="" className="sr-only peer" checked={toggle === 'asc'} onChange={handleToggle} />
-          <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"  >
-          </div>
-          <span className="ml-3 text-sm font-medium text-white-900 dark:text-white-300">Sort by {toggle === 'asc' ? 'Ascending' : 'Descending'}</span>
-        </label>
-      </div>
-      {/* toggle for sorting ends */}
-
+      <Toggle toggle={toggle} handleToggle={handleToggle} />
       <div className='filter-card-container'>
         <div className='filter-container'>
           <button id="dropdownSearchButton" data-dropdown-toggle="dropdownSearch" className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">Select Breeds <svg className="w-2.5 h-2.5 ml-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
